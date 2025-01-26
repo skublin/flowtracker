884a8d0f897e41e5129bbed5f7373aa04c996a69
Szymon Kublin <szymonkublin@gmail.com>
2025-01-26 16:12:41 +0100

    Fix - potential bugs with response type

--- src/features/workspaces/schemas.ts
+++ src/features/workspaces/schemas.ts
@@ -8,3 +8,12 @@ export const createWorkspaceSchema = z.object({
     ])
     .optional(),
 });
+
+export const updateWorkspaceSchema = z.object({
+    name: z.string().trim().min(1, "Enter at least one character").optional(),
+    image: z.union([
+        z.instanceof(File),
+        z.string().transform((value) => value === "" ? undefined : value),
+    ])
+    .optional(),
+});
