3e8100a2c32d98550c11cd75a5e0dd42e5165d24
Szymon Kublin <szymonkublin@gmail.com>
2025-01-28 11:08:48 +0100

    Feature - added error handling and error page

--- src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
@@ -1,6 +1,12 @@
+import Link from "next/link";
+import { PencilIcon } from "lucide-react";
 import { redirect } from "next/navigation";
 
+import { Button } from "@/components/ui/button";
+
 import { getCurrent } from "@/features/auth/queries";
+import { getProject } from "@/features/projects/queries";
+import { ProjectAvatar } from "@/features/projects/components/project-avatar";
 
 interface ProjectIdPageProps {
     params: Promise<{ projectId: string }>;
@@ -12,9 +18,38 @@ const ProjectIdPage = async (props: ProjectIdPageProps) => {
 
     if (!user) redirect("/sign-in");
 
+    const initialValues = await getProject({
+        projectId: params.projectId,
+    });
+
+    if (!initialValues) {
+        throw new Error("Project not found");
+    }
+
     return (
-        <div>
-            ProjectId: {params.projectId}
+        <div className="flex flex-col gap-y-4">
+            <div className="flex items-center justify-between">
+                <div className="flex items-center gap-x-2">
+                    <ProjectAvatar
+                        name={initialValues.name}
+                        image={initialValues.imageUrl}
+                        className="size-8"
+                    />
+                    <p className="text-lg font-semibold">{initialValues.name}</p>
+                </div>
+                <div>
+                    <Button 
+                        asChild
+                        variant="secondary" 
+                        size="sm"
+                    >
+                        <Link href={`/workspaces/${initialValues.workspaceId}/projects/${initialValues.$id}/settings`}>
+                            <PencilIcon className="size-4 mr-2" />
+                            Edit Project
+                        </Link>
+                    </Button>
+                </div>
+            </div>
         </div>
     );
 };
