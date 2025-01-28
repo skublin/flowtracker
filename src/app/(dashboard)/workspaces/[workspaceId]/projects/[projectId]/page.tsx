38e651636ea303cfa4cd4a3cc8a6812ff80927ac
Szymon Kublin <szymonkublin@gmail.com>
2025-01-28 09:33:12 +0100

    Fix - warning with await params

--- src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
@@ -3,12 +3,11 @@ import { redirect } from "next/navigation";
 import { getCurrent } from "@/features/auth/queries";
 
 interface ProjectIdPageProps {
-    params: { projectId: string };
+    params: Promise<{ projectId: string }>;
 };
 
-const ProjectIdPage = async ({
-    params,
-}: ProjectIdPageProps) => {
+const ProjectIdPage = async (props: ProjectIdPageProps) => {
+    const params = await props.params;
     const user = await getCurrent();
 
     if (!user) redirect("/sign-in");
