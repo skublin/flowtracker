87fa0b028318f86a4e2fd1d486e6cdc9bf7ae4c3
Szymon Kublin <szymonkublin@gmail.com>
2025-02-05 11:27:31 +0100

    Fix - minor bugs resolved

--- src/features/auth/api/use-logout.ts
+++ src/features/auth/api/use-logout.ts
@@ -27,8 +27,7 @@ export const useLogout = () => {
         onSuccess: () => {
             toast.success("Logged out")
             router.refresh();
-            queryClient.invalidateQueries({ queryKey: ["current"] });
-            queryClient.invalidateQueries({ queryKey: ["workspaces"] });
+            queryClient.invalidateQueries();
         },
         onError: () => {
             toast.error("Failed to log out");
